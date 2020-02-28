6618023e2edf2b9c5e171cbb5551a54258c75fca
Kiryl Volkau <volkaukiryl@gmail.com>
2020-02-28 22:03:32 +0100

    Initial Commit

---
+++ Tinder2.0API/Tinder2.0API/Controllers/ValueController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tinder2API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        //GET
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        //POST
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+
+    }
+}
---
+++ Tinder2.0API/Tinder2.0API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Tinder2API
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
