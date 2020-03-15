9f7942e53ea171641ae5b6f0e1d4f71bbfcf9c53
Kiryl Volkau <volkaukiryl@gmail.com>
2020-03-15 14:27:05 +0100

    Angular Error Handling added

--- Tinder-Two/src/app/_services/auth.service.ts
+++ Tinder-Two/src/app/_services/auth.service.ts
@@ -6,21 +6,21 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = "http://localhost:5000/api/auth/";
+  baseUrl = 'http://localhost:5000/api/auth/';
   constructor(private http: HttpClient) { }
 
-  login(model: any){
-  return this.http.post(this.baseUrl + "login", model).pipe(
-    map((response:any)=> {
+  login(model: any) {
+  return this.http.post(this.baseUrl + 'login', model).pipe(
+    map((response: any) => {
       const user = response;
-      if( user ) {
-        localStorage.setItem('token', user.token)
+      if ( user ) {
+        localStorage.setItem('token', user.token);
       }
     })
-  )
+  );
   }
 
-  register(model: any){
+  register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
   }
 }
--- Tinder2.0API/Tinder2.0API/Controllers/AuthController.cs
+++ Tinder2.0API/Tinder2.0API/Controllers/AuthController.cs
@@ -49,7 +49,6 @@ namespace Tinder2._0API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("LOOOOOL");
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
