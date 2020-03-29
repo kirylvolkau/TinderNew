ab6c09553f11b8c1aa663288d976a1660b969f94
Kiryl Volkau <volkaukiryl@gmail.com>
2020-03-29 11:30:39 +0200

    design examples added

--- Tinder2.0API/Tinder2.0API/Data/DataContext.cs
+++ Tinder2.0API/Tinder2.0API/Data/DataContext.cs
@@ -10,5 +10,6 @@ namespace Tinder2API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
